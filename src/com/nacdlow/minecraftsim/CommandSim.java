144628e955251902ab8f2b28eaeab5a525229140
Humaid AlQassimi <me@humaidq.ae>
2020-01-26 02:46:07 +0000

    Add more data in /sim command. And fix default config.

--- src/com/nacdlow/minecraftsim/CommandSim.java
+++ src/com/nacdlow/minecraftsim/CommandSim.java
@@ -4,12 +4,26 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.json.simple.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class CommandSim implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         sender.sendMessage(ChatColor.RED + "== " + ChatColor.AQUA + "Nacdlow Simulation Status" + ChatColor.RED + " ==");
-        sender.sendMessage("Time: " + String.valueOf((Long) APIChecker.apiData.get("current_time")));
+        Date date = new java.util.Date((Long) APIChecker.apiData.get("current_time")*1000L);
+        SimpleDateFormat dateFormat = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        sender.sendMessage("Simulation Time: " + dateFormat.format(date));
+        JSONObject weather = (JSONObject) APIChecker.apiData.get("weather");
+        JSONObject home = (JSONObject) APIChecker.apiData.get("home");
+
+        sender.sendMessage("Outdoor Temperature: "+home.get("power_gen_rate") +" kWh of " + home.get("solar_max_power") +" max");
+        sender.sendMessage("Outdoor Temperature: " + weather.get("outdoor_temp"));
+        sender.sendMessage("Outdoor Humidity: " +  weather.get("humidity"));
+        sender.sendMessage("Cloud Cover: " +  weather.get("cloud_cover"));
         return true;
     }
 }
